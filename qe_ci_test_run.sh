c8744198f955fc82e50b444dffd3bc9e23e47f70
liornoy <lnoy@redhat.com>
2022-10-26 15:01:55 +0300

    fixup

--- qe_ci_test_run.sh
+++ qe_ci_test_run.sh
@@ -3,14 +3,12 @@ echo "Starting setup for testing ibgp-single-hop container"
 
 frr_image="frr:v7.5.1"
 metallb_path="/home/lnoy/go/src/github.com/liornoy/metallb"
-SKIP="IPV6|DUALSTACK|metrics"
 script_path=${PWD}
 
 cp -r ibgp-single-hop ${metallb_path}/e2etest
 cd ${metallb_path}
 docker run -d --privileged  --name ibgp-single-hop --network kind --rm -v ${PWD}/e2etest/ibgp-single-hop/:/etc/frr frrouting/${frr_image}
-export USE_IBGP_SINGLE_HOP=true
-invoke e2etest
+invoke e2etest --external-containers="ibgp-single-hop"
 
 echo "Starting setup for testing multi-hop scenrio"
 cd ${script_path}
@@ -26,9 +24,4 @@ docker run -d --privileged  --name ebgp-single-hop --network kind --rm -v ${PWD}
 docker run -d --privileged  --name ebgp-multi-hop --network multi-hop-net --rm -v ${PWD}/e2etest/ebgp-multi-hop/:/etc/frr frrouting/${frr_image}
 docker run -d --privileged  --name ibgp-multi-hop --network multi-hop-net --rm -v ${PWD}/e2etest/ibgp-multi-hop/:/etc/frr frrouting/${frr_image}
 
-
-unset USE_IBGP_SINGLE_HOP
-export USE_EBGP_SINGLE_HOP=true
-export USE_EBGP_MULTI_HOP=true
-export USE_IBGP_MULTI_HOP=true
-invoke e2etest
+invoke e2etest --external-containers="ebgp-single-hop,ebgp-multi-hop,ibgp-multi-hop"
