cb857aa88e24f46ae1836ec0e3b79279dac25b9a
liornoy <lnoy@redhat.com>
2022-10-26 15:58:18 +0300

    fix cleanup

--- clean.sh
+++ clean.sh
@@ -3,7 +3,3 @@ echo "Starting clean up script"
 docker rm -f ibgp-single-hop ibgp-multi-hop ebgp-single-hop ebgp-multi-hop
 rm -rf ${metallb_path}/e2etest/ibgp-single-hop ${metallb_path}/e2etest/ibgp-multi-hop ${metallb_path}/e2etest/ebgp-single-hop ${metallb_path}/e2etest/ebgp-multi-hop
 docker network remove multi-hop-net
-unset USE_IBGP_SINGLE_HOP
-unset USE_IBGP_MULTI_HOP
-unset USE_EBGP_MULTI_HOP
-unset USE_EBGP_SINGLE_HOP
